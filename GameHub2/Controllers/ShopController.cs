f2a331764343b6c7ef6a814c59efd8c2e60186d3
abraham107 <abrahamsamuel107@gmail.com>
2022-11-09 14:48:37 -0500

    third commit

--- GameHub2/Controllers/ShopController.cs
+++ GameHub2/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameHub2.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameHub2.Controllers
 {
@@ -6,6 +7,29 @@ namespace GameHub2.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Category(string Name)
+        {
+            // ensure some category name was passed to
+            if (Name == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // get the name param from url and store in view data object so we can display it
+            ViewData["Category"] = Name;
+
+            // use product model to create a list of products in memory for display
+            var products = new List<Product>();
+
+            // use a loop to make a list of products
+            for (var i =1; i<6; i++)
+            {
+                products.Add(new Product {  ProductId = i, Name = "Product" + i.ToString(), Price = 10 + i });
+            }
+
             return View();
         }
     }
