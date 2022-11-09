790b7cd40afd4b9c0792d1dc2f0ef79d13b427e7
abraham107 <abrahamsamuel107@gmail.com>
2022-11-09 12:15:13 -0500

    second commit

---
+++ GameHub2/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameHub2.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
